be7d3d2f1bc458da0dd08d1efa5bf567a5977f28
thepaperpilot <thepaperpilot@gmail.com>
2025-04-06 11:46:54 -0500

    Moved auth to websocket layer and added unit tests

---
+++ src/index.test.ts
@@ -0,0 +1,88 @@
+import { assertExists, assertRejects, fail } from "@std/assert";
+import {
+  addUser,
+  closeUsersKv,
+  getUsersKv,
+  removeUser,
+  startServer,
+} from "./index.ts";
+import { assertEquals } from "@std/assert/equals";
+
+Deno.test("user operations", async (t) => {
+  const username = "fakeUsername";
+  const password = "somePassword";
+
+  await t.step("add user", async () => {
+    await addUser(username, password);
+    assertExists((await getUsersKv()).get(["users", username]));
+  });
+
+  await t.step("remove user", async () => {
+    await removeUser(username);
+    assertEquals(
+      (await (await getUsersKv()).get(["users", username])).value,
+      null,
+    );
+  });
+
+  closeUsersKv();
+});
+
+async function testCredentials(
+  address: string,
+  username: string,
+  password: string,
+) {
+  const socket = new WebSocket("ws://localhost:8000", [
+    "authorization",
+    `${username}-${password}`,
+  ]);
+  await new Promise((resolve, reject) => {
+    socket.addEventListener("open", resolve);
+    socket.addEventListener("error", reject);
+  }).finally(() => socket.close());
+}
+
+Deno.test("socket connection", async (t) => {
+  const username = "validUsername";
+  const password = "validPassword";
+
+  const otherUsername = "invalidUsername";
+  const otherPassword = "invalidPassword";
+
+  await addUser(username, password);
+  const server = await startServer({
+    port: 8000,
+    dbFile: "./data/test.sqlite",
+  });
+  const { hostname, port } = server.addr;
+  const address = `ws://${hostname}:${port}`;
+
+  await t.step("authorized connection", async () => {
+    await testCredentials(address, username, password);
+  });
+
+  await t.step("unauthorized connection", async () => {
+    const socket = new WebSocket(address);
+    await new Promise((resolve, reject) => {
+      socket.addEventListener("open", reject);
+      socket.addEventListener("error", resolve);
+    });
+  });
+
+  await t.step("unrecognized credentials", async () => {
+    await assertRejects(() =>
+      testCredentials(address, otherUsername, otherPassword)
+    );
+  });
+
+  await t.step("incorrect credentials", async () => {
+    await assertRejects(() =>
+      testCredentials(address, username, otherPassword)
+    );
+  });
+
+  await removeUser(username);
+  closeUsersKv();
+  await server.shutdown();
+});
---
+++ src/index.ts
@@ -0,0 +1,99 @@
+import { StorageManager } from "@muni-town/leaf/storage";
+import { denoKvStorageAdapter } from "@muni-town/leaf/storage/deno-kv";
+import { SuperPeer1 } from "@muni-town/leaf/sync1";
+import { handleRequest as handlePeerRequest } from "@muni-town/leaf/sync1/ws-server";
+import bcrypt from "bcrypt";
+import { error } from "itty-router";
+
+let usersKv: Deno.Kv | undefined;
+export async function getUsersKv() {
+  usersKv ??= await Deno.openKv(Deno.env.get("USERS_DB_FILE") || "./users.db");
+  return usersKv;
+}
+
+export function closeUsersKv() {
+  usersKv?.close();
+  usersKv = undefined;
+}
+
+export async function addUser(username: string, password: string) {
+  const kv = await getUsersKv();
+  const existingUser = await kv.get<string>(["users", username]);
+
+  if (existingUser.value) {
+    console.log(`User "${username}" already exists.`);
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await kv.set(["users", username], hashedPassword);
+}
+
+export async function removeUser(username: string) {
+  await (await getUsersKv()).delete(["users", username]);
+}
+
+export async function verifyUser(username: string, password: string) {
+  const kv = await getUsersKv();
+  const existingUser = await kv.get<string>(["users", username]);
+
+  if (!existingUser.value) {
+    // User does not exist.
+    return false;
+  }
+
+  const hashedPassword = existingUser.value;
+  return bcrypt.compare(password, hashedPassword);
+}
+
+/**
+ * Handle an HTTP request, upgrading it to a websocket connection, and hosting the super peer binary
+ * protocol over it.
+ */
+export async function handleRequest(
+  superPeer: SuperPeer1,
+  request: Request,
+): Promise<Response> {
+  // Get the authorization token from the header
+  const credentials = request.headers
+    .get("Sec-WebSocket-Protocol")
+    ?.split("authorization,")[1]
+    ?.trim();
+  const [username, password] = credentials?.split("-") ?? [];
+  const authenticated = await verifyUser(username ?? "", password ?? "");
+  if (!authenticated) {
+    return error(401, "You are not authorized to connect to this sync server");
+  }
+
+  return handlePeerRequest(superPeer, request);
+}
+
+/** Start a websocket sync server */
+export async function startServer(opts: { port: number; dbFile: string }) {
+  const db = await Deno.openKv(opts.dbFile);
+
+  const superPeer = new SuperPeer1(
+    new StorageManager(denoKvStorageAdapter(db)),
+  );
+  const server = Deno.serve({ port: opts.port }, (req) => {
+    return handleRequest(superPeer, req);
+  });
+  server.finished.then(() => db.close());
+
+  return server;
+}
+
+if (import.meta.main) {
+  const [command, ...args] = Deno.args[0];
+  if (command === "add_user" && args.length >= 2) {
+    addUser(args[0], args[1]);
+  } else if (command === "remove_user" && args.length >= 1) {
+    removeUser(args[0]);
+  } else {
+    console.log("port", Deno.env.get("PORT"));
+    const port = parseInt(Deno.env.get("PORT") || "8000");
+    const dbFile = Deno.env.get("DB_FILE") || "./data.sqlite";
+
+    startServer({ port, dbFile });
+  }
+}
